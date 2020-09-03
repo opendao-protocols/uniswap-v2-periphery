a923f556835ba0a52e72443e5a03f1f6d05bac0e
William Griffiths <willjgriff@gmail.com>
2020-09-03 16:44:44 +0100

    Convert to truffle project. Update Library init code hash.

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# Uniswap V2
+# Uniswap V2 Periphery
 
-[![Actions Status](https://github.com/Uniswap/uniswap-v2-periphery/workflows/CI/badge.svg)](https://github.com/Uniswap/uniswap-v2-periphery/actions)
-[![npm](https://img.shields.io/npm/v/@uniswap/v2-periphery?style=flat-square)](https://npmjs.com/package/@uniswap/v2-periphery)
+See https://github.com/1Hive/uniswap-v2-core for deployment instructions
 
-In-depth documentation on Uniswap V2 is available at [uniswap.org](https://uniswap.org/docs).
 
-The built contract artifacts can be browsed via [unpkg.com](https://unpkg.com/browse/@uniswap/v2-periphery@latest/).
-
-# Local Development
-
-The following assumes the use of `node@>=10`.
-
-## Install Dependencies
-
-`yarn`
-
-## Compile Contracts
-
-`yarn compile`
-
-## Run Tests
-
-`yarn test`
---
+++ migrations/2_deploy.js
@@ -0,0 +1,32 @@
+const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
+const UniswapV2Router01 = artifacts.require("UniswapV2Router01");
+const ERC20 = artifacts.require("ERC20")
+const WETH = artifacts.require("WETH9")
+
+const FACTORY_ADDRESS = "0x8fa8585fCA614EE4F7a46E1bDdE4F2d45B065505"
+const WRAPPED_ETH = "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d"
+
+const toBn = (value) => new web3.utils.toBN(value)
+const toBnWithDecimals = (x, y = 18) => toBn((toBn(x).mul(toBn(10).pow(toBn(y)))).toString())
+
+module.exports = async (deployer) => {
+  // await deployer.deploy(ERC20, toBnWithDecimals(1000))
+  // const token1 = await ERC20.at(ERC20.address)
+  // console.log(`Token 1 deployed: ${token1.address}`)
+  //
+  // await deployer.deploy(ERC20, toBnWithDecimals(1000))
+  // const token2 = await ERC20.at(ERC20.address)
+  // console.log(`Token 2 deployed: ${token2.address}`)
+  //
+  // await deployer.deploy(WETH)
+  // console.log(`WETH deployed: ${WETH.address}`)
+  //
+  // await deployer.deploy(UniswapV2Router02, FACTORY_ADDRESS, WETH.address);
+  //
+  // await token1.approve(UniswapV2Router02.address, toBnWithDecimals(1000))
+  // await token2.approve(UniswapV2Router02.address, toBnWithDecimals(1000))
+  // console.log(`Approved both tokens for Router to spend`
+
+  await deployer.deploy(UniswapV2Router02, FACTORY_ADDRESS, WRAPPED_ETH);
+
+};
